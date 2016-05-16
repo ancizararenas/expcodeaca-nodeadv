d21994d93f1afeba548225cc4db48528e42ac62b
Daniel Leahy <dleahy@LONC02QG0NSG8WL.sea.corp.expecn.com>
2016-05-16 10:29:50 +0100

    dan - homework module three

--- bin/server.js
+++ bin/server.js
@@ -1,5 +1,7 @@
 var mongoose    = require('mongoose');
 var express     = require('express');
+var bodyParser  = require('body-parser');
+var routes      = require('../lib/routes');
 var app         = express();
 var mongoURI    = process.env.MONGO || 'mongodb://localhost:27017/nodeadvca';
 var log         = require('../middleware/logger').child({
@@ -8,9 +10,33 @@ var log         = require('../middleware/logger').child({
 });
 
 // set port for server
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8999;
 
 // connect app to database
 mongoose.connect(mongoURI);
 
-app.listen(port);
+mongoose.connection.on('error', function(err){
+  log.error('error connecting to DB :%s', mongoURI, err);
+
+})
+
+mongoose.connection.on('open', function(err){
+  log.info('successfully connecting to DB :%s', mongoURI);
+  instantiateServer()
+})
+
+
+function instantiateServer(){
+
+  app.use(bodyParser.urlencoded({extended: true}));
+  app.use(bodyParser.json());
+
+  var router = express.Router();
+  routes.register(router);
+
+  app.use('/', router);
+  app.set('port', port);
+  app.listen(port);
+  console.log('listenin on port : %s', port);
+
+};
