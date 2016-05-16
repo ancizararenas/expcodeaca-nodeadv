61e552c110a9d72b9fc3bda49b6798e3041e1697
Xueqi Chen <xuechen@LONC02PR0KAG8WL.sea.corp.expecn.com>
2016-05-16 11:19:16 +0100

    my homework

--- app.js
+++ app.js
@@ -1,13 +1,1 @@
 
-
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-
-app.get('/blog', function(request, response) {
-    response.send("This would be some HTML");
-});
-
-app.get('/api', function(request, response) {
-    response.send({name:"ancizar", age:18});
-});
--- bin/server.js
+++ bin/server.js
@@ -1,16 +1,43 @@
 var mongoose    = require('mongoose');
 var express     = require('express');
+var bodyParser  = require('body-parser');
 var app         = express();
+var router      = express.Router();
+var routes      = require('../lib/routes');
 var mongoURI    = process.env.MONGO || 'mongodb://localhost:27017/nodeadvca';
 var log         = require('../middleware/logger').child({
   level : 'info',
   component : '[BIN-SERVER]'
 });
 
-// set port for server
-var port = process.env.PORT || 3000;
-
 // connect app to database
+log.info("Connecting to DB %s", mongoURI);
 mongoose.connect(mongoURI);
 
-app.listen(port);
+mongoose.connection.on('connected', function () {
+  log.info("Successfully connected to DB %s", mongoURI);
+  startServer();
+});
+mongoose.connection.on('error', function (err) {
+  log.error("Error connecting to DB %s", mongoURI, err);
+});
+
+// middleware to use for all requests
+router.use(function(req, res, next) {
+  log.info({req : req},
+    {message : req.method + ' - request being made to - ' + req.originalUrl});
+  next(); // make sure we go to the next routes and don't stop here
+});
+
+function startServer() {
+  // set port for server
+  var port = process.env.PORT || 3000;
+  app.use(bodyParser.urlencoded({extended: true}));
+  app.use(bodyParser.json());
+
+  routes.register(router);
+  app.use('/', router);
+
+  app.listen(port);
+  log.info("Server started on port %s", port);
+}
