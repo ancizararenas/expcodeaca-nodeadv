8b80762fa708b21024126ba64ddb7c036956695c
Dakshesh Hemendrarai <dhemendrarai@expedia.com>
2016-05-18 18:59:39 +0100

    daks - week 3 homework, using routes

--- bin/server.js
+++ bin/server.js
@@ -1,16 +1,43 @@
 var mongoose    = require('mongoose');
 var express     = require('express');
 var app         = express();
+var bodyParser  = require('body-parser');
 var mongoURI    = process.env.MONGO || 'mongodb://localhost:27017/nodeadvca';
+var routes      = require('../lib/routes');
 var log         = require('../middleware/logger').child({
   level : 'info',
   component : '[BIN-SERVER]'
 });
 
-// set port for server
-var port = process.env.PORT || 3000;
+function instantiateServer() {
+	// configure app to use bodyParser() this will let us get the data from a POST & PUT requests
+	app.use(bodyParser.urlencoded({ extended: true }));
+	app.use(bodyParser.json());
+
+	// set port for server
+	var port = process.env.PORT || 3000;
+
+	app.use(routes);
+	
+	routes.use(function(req, res, next) {
+		log.info({ req: req },
+				{ message: req.method + ' - request being made to - ' + req.originalUrl });
+	});
+
+	app.listen(port);
+
+	log.info('Server started and listening on port: %s', port);
+}
 
 // connect app to database
+log.info('Preparing to initiate connection to DB');
 mongoose.connect(mongoURI);
 
-app.listen(port);
+mongoose.connection.on('error', function(err) {
+	log.info('Error connecting to DB: %s', mongoURI, err);
+});
+
+mongoose.connection.on('open', function() {
+	log.info('Successfully connected to DB: %s', mongoURI);
+	return instantiateServer();
+});
